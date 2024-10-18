c88b8d04dd1999ad6f92eca42c409d0160458100
alexandra <lisencoalexandra@gmail.com>
2024-10-18 22:31:30 +0300

    Пробник

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import ScrollRestoration from "./components/ScrollRestoration";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
-import Navbar from "./components/UI/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import Administration from "./pages/Administration";
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
+            <Administration/>
             <ScrollRestoration/>
             <AppRouter/>
         </BrowserRouter>
