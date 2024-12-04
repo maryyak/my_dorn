27e808ee5220c36cdf67e8210554eec50eb611fb
Alexandra <lisencoalexandra@gmail.com>
2024-12-04 11:59:36 +0300

    лк

--- src/components/Navbar/NavbarAuthorized.jsx
+++ src/components/Navbar/NavbarAuthorized.jsx
@@ -1,39 +1,61 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import UserProfile from "../UI/UserProfile/UserProfile";
+import BackButton from "../UI/BackButton/BackButton";
 
 const NavbarAuthorized = ({userData}) => {
     return (
-
-            <div className="navbar row-container">
+        <>
+            <div className="navbar__auth row-container">
                 <div className="container navbar__container row-container">
-                        <div className="navbar__links row-container">
-                            <Link to="/" className="logo">
-                                <img src="/assets/images/logo.png" alt="logo"/>
-                            </Link>
-                            <div className="navbar__col-container">
+                    <div className="navbar__nav row-container">
+                        <Link to="/" className="logo">
+                            <img src="/assets/images/logo.png" alt="logo"/>
+                        </Link>
+                        <div className="navbar__col-container col-container">
+                            {userData.userType === 'resident' &&
                                 <div className="navbar__heading">Личный кабинет проживающего</div>
-                                <div className="navbar__links row-container">
-                                    <Link to="/rules" className="navbar__link">Правила проживания</Link>
-                                    <Link to="/administration" className="navbar__link">Состав администрации</Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="user__navbar row-container">
-                            <div className="user__notifications">
-                                <img src="/assets/images/notifications.png" alt="notifications"/>
-                            </div>
-                            <div className="user__info">
-                                <div className="user__name">
-                                    {userData.firstName}
-                                    &nbsp;{userData.lastName}
-                                </div>
-                                <div className="user__email">
-                                </div>
+                            }
+                            {userData.userType === 'dormHead' &&
+                                <div className="navbar__heading">Личный кабинет старосты</div>
+                            }
+                            {userData.userType === 'commandant' &&
+                                <div className="navbar__heading">Личный кабинет комменданта</div>
+                            }
+                            <div className="navbar__links row-container">
+                                <Link to="/rules" className="navbar__link">Правила проживания</Link>
+                                <Link to="/administration" className="navbar__link">Состав администрации</Link>
                             </div>
                         </div>
                     </div>
+                    <UserProfile/>
+                </div>
             </div>
-
+            <div className="subnavbar row-container">
+                <div className="subnavbar__container container row-container">
+                    {(userData.userType === 'resident' || userData.userType === 'dormHead') &&
+                        <>
+                            <Link to="/duty-schedule" className="subnavbar__link">График дежурства</Link>
+                            <Link to="/submit-application" className="subnavbar__link">Подать заявление</Link>
+                            <Link to="/events" className="subnavbar__link">Мероприятия</Link>
+                        </>
+                    }
+                    {userData.userType === 'commandant' &&
+                        <>
+                            <Link to="/communicate-residents" className="subnavbar__link">Связь с проживающими</Link>
+                            <Link to="/applications" className="subnavbar__link">Заявления</Link>
+                            <Link to="/events" className="subnavbar__link">Мероприятия</Link>
+                            <Link to="/decommission" className="subnavbar__link">Снятие с учета</Link>
+                        </>
+                    }
+                </div>
+            </div>
+            <div className="navbar-back row-container">
+                <div className="navbar-back__container container row-container">
+                    <BackButton/>
+                </div>
+            </div>
+        </>
     );
 };
 export default NavbarAuthorized;
--- src/components/SupportBlock.jsx
+++ src/components/SupportBlock.jsx
@@ -2,13 +2,13 @@ import React from "react";
 const SupportBlock = () => {
     return (
         <div className="support-block col-container">
-            <div className="support-header row-container">
-                <div className="support-img">
+            <div className="support-block__header row-container">
+                <div className="support-block__img">
                     <img src="/assets/images/plane-icon.png" alt="Самолетик"/>
                 </div>
-                <span className="support-header__text">Связаться с поддержкой</span>
+                <span className="support-block__text">Связаться с поддержкой</span>
             </div>
-            <textarea className="support-input" placeholder="Ваш вопрос"></textarea>
+            <textarea className="support-block__input" placeholder="Ваш вопрос"></textarea>
             <div className="support-block__btn">
                 <input className="support-block__submit" type="submit" value="Отправить"/>
             </div>
