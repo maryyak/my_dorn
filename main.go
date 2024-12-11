2df098b681b2d40e164a0aac640c1244318754c7
Artyom Metel <metelartem22@gmail.com>
2024-12-11 23:03:13 +0300

    Parser Vk news

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"dorn_project/server/database"
+	"dorn_project/server/utilits"
 	"log"
 	"net/http"
 )
 
 func main() {
-
 	database.Connect()
+
+	utilits.VkParsing()
 
 	log.Print("Connecting to localhost:8080....")
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err == nil {
 		log.Fatal("Cant connect to url - localhost:8080")
 	}
+
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,13 +4,13 @@ const NewsItem = (props) => {
     return (
         <div className="news-item">
             <div className="img-container news-item__img">
-                <img src={props.data.image} alt="news-img"/>
+                <img src={props.data.url} alt="news-img"/>
             </div>
             <div className="news-item__content">
-                <div className="news-item__title">{props.data.title}</div>
+                <div className="news-item__title">{props.data.date}</div>
                 <div className="news-item__text">{props.data.text}</div>
                 <div className="news-item__btn">
-                    <a className="news-item__href" href={props.data.href}>Читать полностью</a>
+                    <a className="news-item__href" href={props.data.id}>Читать полностью</a>
                 </div>
             </div>
         </div>
