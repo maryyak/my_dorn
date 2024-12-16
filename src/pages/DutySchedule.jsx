21b1d990b934f17d06c2ff0e2e1fc1ca1210a777
Alexandra <lisencoalexandra@gmail.com>
2024-12-16 11:27:33 +0300

    правки графика дежурств

--- src/pages/DutySchedule.jsx
+++ src/pages/DutySchedule.jsx
@@ -1,15 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../components/UserProvider";
 
 const DutySchedule = () => {
     const { userData } = useContext(UserContext);
     const [photo, setPhoto] = useState('/assets/images/dutySchedule.jpg'); // Начальное изображение
 
+    // Загрузка сохраненного изображения из localStorage при монтировании
+    useEffect(() => {
+        const savedPhoto = localStorage.getItem("dutySchedulePhoto");
+        if (savedPhoto) {
+            setPhoto(savedPhoto); // Если картинка есть в localStorage, используем её
+        }
+    }, []);
+
     const handlePhotoUpload = (event) => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = () => setPhoto(reader.result); // Устанавливаем изображение в стейт
+            reader.onload = () => {
+                const result = reader.result;
+                setPhoto(result); // Устанавливаем изображение в стейт
+                localStorage.setItem("dutySchedulePhoto", result); // Сохраняем изображение в localStorage
+            };
             reader.readAsDataURL(file);
         }
     };
@@ -17,26 +29,26 @@ const DutySchedule = () => {
     return (
         <div className="App">
             <div className="container">
-                {userData.userType === 'resident' && (
+                {userData.userType === "resident" && (
                     <>
                         <div className="rules-title">Текущее расписание</div>
                         <img src={photo} alt="schedule" />
                     </>
                 )}
-                {userData.userType === 'dormHead' && (
+                {userData.userType === "dormHead" && (
                     <>
                         <div className="rules-title">Текущее расписание</div>
                         <img
                             src={photo}
                             alt="User Schedule"
-                            style={{cursor: 'pointer'}}
-                            onClick={() => document.getElementById('photoUpload').click()}
+                            style={{ cursor: "pointer" }}
+                            onClick={() => document.getElementById("photoUpload").click()}
                         />
                         <input
                             type="file"
                             id="photoUpload"
                             accept="image/*"
-                            style={{display: 'none'}}
+                            style={{ display: "none" }}
                             onChange={handlePhotoUpload}
                         />
                     </>
